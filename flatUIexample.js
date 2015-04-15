a61abbf7f2d859e5168c0418e89d0c014fa2886e
Jorge Ortega Morata <j.ortega.morata@gmail.com>
2015-04-15 20:23:18 +0200

    incluidas urls de imagenes

--- flatUIexample.js
+++ flatUIexample.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 	flat1 = FlatUI.create('iMac');
-	flat1.JqueryObj.appendTo('body');
-	FlatUI.setScreen(flat1,'captura.jpg');
+	FlatUI.setScreen(flat1,'http://www.smashingbuzz.com/wp-content/uploads/2013/07/Flat-UI.4.jpg');
 	FlatUI.setId(flat1,'imacImage');
+	flat1.JqueryObj.appendTo('body');
 	flat2 = FlatUI.create('Macbook');
-	flat2.JqueryObj.appendTo('body');
 	FlatUI.setScreen(flat2,'http://flatdsgn.com/wp-content/uploads/2013/08/Long-Shadow-Flat-UI-Kit2.jpg');
 	FlatUI.setId(flat2,'macbookImage');
+	flat2.JqueryObj.appendTo('body');
 });
